eb143b75a441a02144e5f77b6c63c08911526ee3
Takeuchi2049 <take14mwam@gmail.com>
2021-05-27 17:37:49 +0900

    チーム演習　カート機能

---
+++ CartController.java
@@ -0,0 +1,75 @@
+package jp.co.internous.gaia.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import jp.co.internous.gaia.model.domain.TblCart;
+import jp.co.internous.gaia.model.domain.dto.CartDto;
+import jp.co.internous.gaia.model.form.CartForm;
+import jp.co.internous.gaia.model.mapper.TblCartMapper;
+import jp.co.internous.gaia.model.session.LoginSession;
+
+@Controller
+@RequestMapping("/gaia/cart")
+public class CartController {
+	
+	@Autowired
+	private TblCartMapper tblCartMapper;
+	
+	@Autowired
+	private LoginSession loginSession;
+	
+	private Gson gson = new Gson();
+	
+	
+	@RequestMapping("/")
+	public String index(Model m) {
+		int userId = loginSession.getLogined() ? loginSession.getUserId() : loginSession.getTmpUserId();
+		List<CartDto> carts = tblCartMapper.findByUserId(userId);
+		m.addAttribute("loginSession", loginSession);
+		m.addAttribute("carts", carts);
+		
+		return "cart";
+	}
+	
+	@RequestMapping("/add")
+	public String addCart(CartForm f, Model m) {
+		int userId = loginSession.getLogined() ? loginSession.getUserId() : loginSession.getTmpUserId();
+		f.setUserId(userId);
+		TblCart cart = new TblCart(f);
+		int result = 0;
+		if (tblCartMapper.findCountByUserIdAndProuductId(userId, f.getProductId()) > 0) {
+			result = tblCartMapper.update(cart);
+		} else {
+			result = tblCartMapper.insert(cart);
+		}
+		
+		if(result > 0) {
+			List<CartDto> carts = tblCartMapper.findByUserId(userId);
+			m.addAttribute("loginSession", loginSession);
+			m.addAttribute("carts", carts);
+		}
+		return "cart";
+	}
+	
+	@SuppressWarnings("unchecked")
+	@ResponseBody
+	@RequestMapping("/delete")
+	public boolean deleteCart (@RequestBody String checkedIdList) {
+		int result = 0;
+		Map<String, List<String>> map = gson.fromJson(checkedIdList, Map.class);
+		List<String> checkedIds = map.get("checkedIdList");
+		result = tblCartMapper.deleteById(checkedIds);
+	
+		return result > 0;
+	}
+}
